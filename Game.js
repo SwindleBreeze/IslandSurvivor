361729b71a832f3ff8b9f27bc35572812eaab339
AljazJus <aljaz.justin@gmail.com>
2022-12-27 18:18:20 +0100

    nwm ka je fora

--- Game.js
+++ Game.js
@@ -1,52 +1,51 @@
-import { Application } from './modules/engine/Application.js';
+import { Application } from "./modules/engine/Application.js";
 
-import { GLTFLoader } from './modules/engine/GLTFLoader.js';
-import { Renderer } from './modules/engine/Renderer.js';
-import { Node } from './modules/engine/Node.js';
-import { GameController } from './modules/game/Controllers/GameController.js';
+import { GLTFLoader } from "./modules/engine/GLTFLoader.js";
+import { Renderer } from "./modules/engine/Renderer.js";
+import { Node } from "./modules/engine/Node.js";
+import { GameController } from "./modules/game/Controllers/GameController.js";
 
 //JUSTI IS GHEJ
-export class Game extends Application{
-    async start(){
-        this.renderer = new Renderer(this.gl)
-        this.Loader = new GLTFLoader();
+export class Game extends Application {
+  async start() {
+    this.renderer = new Renderer(this.gl);
+    this.Loader = new GLTFLoader();
 
-        await this.Loader.load('./scenes/gltf/testIsland.gltf');
+    await this.Loader.load("./scenes/gltf/testIsland.gltf");
 
-        this.camera = await this.Loader.loadNode("Camera")
-        this.light = await this.Loader.loadNode("Light")
-        console.log(this.light)
-        this.scene = await this.Loader.loadScene(this.Loader.defaultScene)
+    this.camera = await this.Loader.loadNode("Camera");
+    this.light = await this.Loader.loadNode("Light");
+    console.log(this.light);
+    this.scene = await this.Loader.loadScene(this.Loader.defaultScene);
 
-        if (!this.scene || !this.camera) {
-            throw new Error('Scene or Camera not present in glTF');
-        }
-
-        if (!this.camera.camera) {
-            throw new Error('Camera node does not contain a camera reference');
-        }
-
-        //Create sun object
-        this.sun = new Node();
-        this.sun.position = [0, 2, 1];
-        this.sun.color = [255, 255, 255];
-        this.sun.intensity = 1;
-        this.sun.attenuation = [0.001, 0, 0.3];
-
-
-        console.log(this.scene)
-        
-        this.renderer.prepareScene(this.scene);
-        this.GameController = new GameController
+    if (!this.scene || !this.camera) {
+      throw new Error("Scene or Camera not present in glTF");
     }
 
-    render()
-    {
-        this.renderer.render(this.scene, this.camera, this.sun);
+    if (!this.camera.camera) {
+      throw new Error("Camera node does not contain a camera reference");
     }
 
-    resize(width, height) {
-        this.camera.camera.aspect = width / height;
-        this.camera.camera.updateProjectionMatrix();
-    }
+    //Create sun object
+    //to ni glih ok sam bomo vidl
+    this.sun = new Node();
+    this.sun.position = [0, 2, 1];
+    this.sun.color = [255, 255, 255];
+    this.sun.intensity = 1;
+    this.sun.attenuation = [0.001, 0, 0.3];
+
+    console.log(this.scene);
+
+    this.renderer.prepareScene(this.scene);
+    this.GameController = new GameController();
+  }
+
+  render() {
+    this.renderer.render(this.scene, this.camera, this.sun);
+  }
+
+  resize(width, height) {
+    this.camera.camera.aspect = width / height;
+    this.camera.camera.updateProjectionMatrix();
+  }
 }
